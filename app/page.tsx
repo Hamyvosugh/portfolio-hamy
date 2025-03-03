2f59aa0e9e1aa95f7134a58630b7b41e65e19633
hamyvosugh <hamy.vosugh@gmail.com>
2025-03-03 23:08:24 +0100

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>it is changed  /// test for auto deploy // after add action 2</li>
+          <li>it is changed  /// test for auto deploy // after add action 2// after change auto</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
